fc66ea15adba3fde28370177ff09864caecc9611
FrancoEnslin <frankzenslin@gmail.com>
2023-03-27 18:20:49 +0200

    Added new card to code base

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -18,75 +18,75 @@ function Services() {
 
 
     return (
-        <div  className="App" id='services'>
-            <h3>Services I Offer</h3>
-            <Container>
-                {/* Stack the columns on mobile by making one full-width and the other half-width */}
-                {/* Row 1 */}
-                <Row>
-                    <Col xs={4} md={4}>
-                        <Card className='Card' id='couples' >
-                            <Card.Body>Couple shoot</Card.Body>
-                            <Card.Img variant="top" src={Image1} alt='Photo' />
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='Card' id='family'>
-                            <Card.Body> Family shoot </Card.Body>
-                            <Card.Img variant="top" src={Image2} alt='Photo'  />
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='Card' id='portrait'>
-                            <Card.Body>Portrait shoot </Card.Body>
-                            <Card.Img variant="top" src={Image3} alt='Photo' />
-                        </Card>
-                    </Col>
-                </Row>
-                {/* Row 2 */}
-                <Row>
-                    <Col xs={4} md={4}>
-                        <Card className='Card' id='friends' >
-                            <Card.Body>Friendship shoot</Card.Body>
-                            <Card.Img variant="top" src={Image1} alt='Photo' />
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='Card' id='events'>
-                            <Card.Body> Events </Card.Body>
-                            <Card.Img variant="top" src={Image2} alt='Photo'  />
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='Card' id='matric'>
-                            <Card.Body> Matric dances </Card.Body>
-                            <Card.Img variant="top" src={Image3} alt='Photo' />
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                <Col xs={4} md={4}>
-                        <Card className='Card' id='formals'>
-                            <Card.Body> University formals </Card.Body>
-                            <Card.Img variant="top" src={Image3} alt='Photo' />
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='Card' id='graduations'>
-                            <Card.Body> Graduations </Card.Body>
-                            <Card.Img variant="top" src={Image3} alt='Photo' />
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='Card' id='engagements'>
-                            <Card.Body> Engagements </Card.Body>
-                            <Card.Img variant="top" src={Image3} alt='Photo' />
-                        </Card>
-                    </Col>
+        // <div  className="App" id='services'>
+        //     <h3>Services I Offer</h3>
+        //     <Container>
+        //         {/* Stack the columns on mobile by making one full-width and the other half-width */}
+        //         {/* Row 1 */}
+        //         <Row>
+        //             <Col xs={4} md={4}>
+        //                 <Card className='Card' id='couples' >
+        //                     <Card.Body>Couple shoot</Card.Body>
+        //                     <Card.Img variant="top" src={Image1} alt='Photo' />
+        //                 </Card>
+        //             </Col>
+        //             <Col xs={4} md={4}>
+        //                 <Card className='Card' id='family'>
+        //                     <Card.Body> Family shoot </Card.Body>
+        //                     <Card.Img variant="top" src={Image2} alt='Photo'  />
+        //                 </Card>
+        //             </Col>
+        //             <Col xs={4} md={4}>
+        //                 <Card className='Card' id='portrait'>
+        //                     <Card.Body>Portrait shoot </Card.Body>
+        //                     <Card.Img variant="top" src={Image3} alt='Photo' />
+        //                 </Card>
+        //             </Col>
+        //         </Row>
+        //         {/* Row 2 */}
+        //         <Row>
+        //             <Col xs={4} md={4}>
+        //                 <Card className='Card' id='friends' >
+        //                     <Card.Body>Friendship shoot</Card.Body>
+        //                     <Card.Img variant="top" src={Image1} alt='Photo' />
+        //                 </Card>
+        //             </Col>
+        //             <Col xs={4} md={4}>
+        //                 <Card className='Card' id='events'>
+        //                     <Card.Body> Events </Card.Body>
+        //                     <Card.Img variant="top" src={Image2} alt='Photo'  />
+        //                 </Card>
+        //             </Col>
+        //             <Col xs={4} md={4}>
+        //                 <Card className='Card' id='matric'>
+        //                     <Card.Body> Matric dances </Card.Body>
+        //                     <Card.Img variant="top" src={Image3} alt='Photo' />
+        //                 </Card>
+        //             </Col>
+        //         </Row>
+        //         <Row>
+        //         <Col xs={4} md={4}>
+        //                 <Card className='Card' id='formals'>
+        //                     <Card.Body> University formals </Card.Body>
+        //                     <Card.Img variant="top" src={Image3} alt='Photo' />
+        //                 </Card>
+        //             </Col>
+        //             <Col xs={4} md={4}>
+        //                 <Card className='Card' id='graduations'>
+        //                     <Card.Body> Graduations </Card.Body>
+        //                     <Card.Img variant="top" src={Image3} alt='Photo' />
+        //                 </Card>
+        //             </Col>
+        //             <Col xs={4} md={4}>
+        //                 <Card className='Card' id='engagements'>
+        //                     <Card.Body> Engagements </Card.Body>
+        //                     <Card.Img variant="top" src={Image3} alt='Photo' />
+        //                 </Card>
+        //             </Col>
 
-                </Row>
-            </Container>
-        </div>
+        //         </Row>
+        //     </Container>
+        // </div>
 
 
 
@@ -102,6 +102,32 @@ function Services() {
         //     </div>
 
         // </div>
+
+        <div class="flip-card"  >
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <p class="title">FLIP CARD</p>
+                    <p>Hover Me</p>
+                </div>
+                <div class="flip-card-back">
+                    <p class="title">BACK</p>
+                    <p>Leave Me</p>
+                </div>
+            </div>
+
+            <div class="flip-card">
+                <div class="flip-card-inner" style={{ display: "inline", marginLeft: "30px" }}>
+                    <div class="flip-card-front">
+                        <p class="title">FLIP CARD</p>
+                        <p>Hover Me</p>
+                    </div>
+                    <div class="flip-card-back">
+                        <p class="title">BACK</p>
+                        <p>Leave Me</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
